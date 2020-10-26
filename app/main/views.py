7f1bf2953736b43566e599462e39eee6e76d092e
StacyWeru <stacyweru@gmail.com>
2020-10-26 08:52:51 +0300

    Added and modified the files

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,19 @@
-from flask import render_template 
+from flask import render_template , flash , redirect , url_for
 from app import main 
 from .forms import LoginForm
 
 # @main.route('/')
 # def index():
 
-@main.route('/login')
+
+
+@main.route('/login' , methods=['GET' , 'POST'])
 def login():
     form = LoginForm()
-    return render_template ('login.html' , title = 'Sign In' , form=form)
+    if form.validate_on_submit():
+        flash ('Login requested for user {} , remember_me={} '.format(
+            form.username.data , form.remember_me.data
+        ))
+        return redirect (url_for('index'))
+
+    return render_template('login.html' , title = 'Sign In' , form = form)
